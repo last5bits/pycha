f4433b0730dbb57c0f6365f046de4c4f618ba808
Alexey Zhikhartsev <last5bits@gmail.com>
2015-06-10 09:00:36 +0300

    Third riddle solved

--- 3/3.py
+++ 3/3.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # http://www.pythonchallenge.com/pc/def/equality.html
+
+import re
+
+f = open("3", "r")
+
+for s in re.findall("[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]", f.read()):
+    print(s, end='')
+
+print()
